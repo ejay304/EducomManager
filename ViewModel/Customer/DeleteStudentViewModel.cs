61ba4d5acc47e740187481182dcc23cd60f77b29
Alain Fresco <alain.fresco@gmail.com>
2014-07-08 14:22:56 +0200

    ajout de la fenêtre de suppression des étudiants

---
+++ ViewModel/Customer/DeleteStudentViewModel.cs
@@ -0,0 +1,44 @@
+﻿using PrototypeEDUCOM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PrototypeEDUCOM.ViewModel.Customer
+{
+    class DeleteStudentViewModel : BaseViewModel
+    {
+        public student student { get; set; }
+        public ICommand cmdArchive { get; set; }
+        public ICommand cmdDelete { get; set; }
+        public int nbrRequest { get; set; }
+        public Action CloseActionDelete { get; set; }
+
+        private ShowCustomerViewModel parentVM;
+
+        public DeleteStudentViewModel(student student,ShowCustomerViewModel parentVM)
+        {
+            this.cmdDelete = new RelayCommand<Object>(actDeleteStudent);
+            this.student = student;
+            this.nbrRequest = student.requests.Count();
+            this.parentVM = parentVM;
+        }
+
+        private void actDeleteStudent(Object o)
+        {
+            for (int i = 0; i < nbrRequest; i++)
+            {
+                db.requests.Remove(student.requests.First());
+            }
+
+            db.students.Remove(student);
+            db.SaveChanges();
+
+            parentVM.students.Remove(student);
+            parentVM.NotifyPropertyChanged("students");
+            this.CloseActionDelete();
+        }
+    }
+}
