be06ed39a38489db38d2d6f00f2f35552df007b4
Alain Fresco <alain.fresco@gmail.com>
2014-07-10 23:14:16 +0200

    Ajout et modification des Programmes

---
+++ ViewModel/Organisation/EditOrganisationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PrototypeEDUCOM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeEDUCOM.ViewModel.Organisation
+{
+    class EditOrganisationViewModel :BaseViewModel
+    {
+        public Action CloseActionFormEdit { get; set; }
+        public EditOrganisationViewModel(organisation organisation) {
+
+            this.CloseActionFormEdit();
+        
+        }
+    }
+}
